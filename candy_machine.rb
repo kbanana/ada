3a5cebfc419119ac71239ebf936d827a54209b41
kbanana <kay.walker@arts.cornish.edu>
2019-09-20 22:59:03 -0700

    Added more comments
    Added more comments to make the code more readable; grouped variables together.

--- candy_machine.rb
+++ candy_machine.rb
@@ -11,28 +11,32 @@ cookie_list = [
     ["H", 3.75, "PB&J Bar"],
 ]
 
-# Print welcome message and ask for the order
-
+# Defines and prints welcome message 
 entry_message = "Welcome to Kayla's Computer Cookie Shop! \n
     (All cookies provided are virutal.) \n
     Here's a list of all our delicious cookies of the day:"
-
 puts entry_message
 
+# defines variables for the cookie list, menu, user money
 cookie_strings = cookie_list.map { |cookie| "%s: $%0.2f %s" % cookie }
 menu_string = cookie_strings.join("\n")
+order_message = "How much money have you got? > $"
+
+# displays the menu
 puts menu_string
 
-order_message = "How much money have you got? > $"
+# asks user for how much money they have
 puts order_message
 money = gets.chomp.to_f
 
+# determines how much money the user has and responds based on high or low evaluation
 if money >= 5.00
     print "$%0.2f!? That's a lot of cookies you can buy!\n" % [money]
 else
     print "$%0.2f? That's all you've got?\n" % [money]
 end
 
+# asks for user order
 puts "What would you like to buy?"
 order = gets.chomp.upcase
 
@@ -42,15 +46,17 @@ cookie_found = false
 cookie_list.each do |cookie|
     if order == cookie[0]
         cookie_found = true
+        
+        # if the user has enough money for the cookie, we give it to them and calculate change
         if money >= cookie[1]
             puts "Great! Here is one %s\n" % [cookie[2]]
             puts "And here is your $%0.2f change." % [money - cookie[1]]
         else
+            # if the user doesn't have enough money
             puts "Sorry, you can't afford a %s\n" % [cookie[2]]
         end
     end
 end
-
 
 # If we didn't end up finding their order in the list, it was invalid.
 if !cookie_found
